7af41481b0d9ae31f53d2a315aecd3b55067cb80
JohnnyBoy <johnnyguerin@outlook.com>
2014-11-05 11:34:13 -0500

    Fuck le .rar

---
+++ src/layer/agents/Main.java
@@ -0,0 +1,22 @@
+package layer.agents;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * 
+ * @author Hamid Mcheick
+ *
+ */
+@SuppressWarnings("deprecation")
+public class Main {
+
+	public static void main(String[] args) {
+		Resource resource = new ClassPathResource("spring.xml");
+		BeanFactory factory = new XmlBeanFactory(resource);
+		MasterAgent ma = (MasterAgent) factory.getBean("MasterAgent");
+		ma.start();
+	}
+}
